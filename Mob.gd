e2119fd90e1216245f194a6f2b820197b7a2b0dc
hankhayes <105171359+hankhayes@users.noreply.github.com>
2023-02-20 13:49:01 -0600

    added handle_hit to Mob

--- Mob.gd
+++ Mob.gd
@@ -1,17 +1,15 @@
 extends Area2D
-
-
-
+onready var player = $Player
+signal mob_killed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-# 
-func _on_Mob_body_entered(body):
-	pass # Replace with function body.
+func handle_hit(body):
+	queue_free()
+	emit_signal("mob killed")
