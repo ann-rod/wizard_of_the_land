c45aa678d7401b78d9f73c0abcf1b1ab8d0d3325
ann-rod <98418671+ann-rod@users.noreply.github.com>
2023-02-26 19:56:18 -0600

    added mob chasing behavior to mob AI

--- characters/enemies/AI.gd
+++ characters/enemies/AI.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 signal state_changed(new_state)
 signal attempt_to_shoot(target_pos)
+signal chase_target(target_pos)
 
 enum State {
 	PATROL,
@@ -12,14 +13,14 @@ onready var player_detection_zone = $PlayerDetectionZone
 var current_state = State.PATROL setget set_state
 var player = null
 
-
 func _process(_delta):
 	match current_state:
 		State.PATROL:
-			pass
+			emit_signal("chase_target", Vector2.ZERO)
 		State.ENGAGE:
 			if(player != null):
 				emit_signal("attempt_to_shoot", player.global_position)
+				emit_signal("chase_target", player.global_position)
 			else:
 				print("in engage state but no weapon or player")
 		_:
@@ -36,3 +37,8 @@ func _on_PlayerDetectionZone_area_entered(area):
 	if(area.is_in_group("player")):
 		set_state(State.ENGAGE)
 		player = area
+
+func _on_PlayerUndetectZone_area_exited(area):
+	if(area.is_in_group("player")):
+		set_state(State.PATROL)
+		player = null
--- characters/enemies/Mob.gd
+++ characters/enemies/Mob.gd
@@ -1,22 +1,45 @@
 extends Area2D
-#onready var player = $Player
-onready var ai = $AI
-onready var attack_cooldown = $MobAttackCooldown
-
-export (PackedScene) var Spell
 
 signal mob_killed
 signal mob_cast_spell(spell, pos, dir)
 
+export (PackedScene) var Spell
+
+onready var ai = $AI
+onready var attack_cooldown = $MobAttackCooldown
+onready var collision_shape = $CollisionShape2D
+
 var MOB_HP = 3
+var MOB_SPEED = 150
+var mob_direction = Vector2.ZERO
+var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
+	collision_shape.disabled = false # enable collisions
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+func _physics_process(delta):
+	var velocity = mob_direction.normalized()*MOB_SPEED
+	
+	if velocity.length() > 0:
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+		
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "side"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y < 0:
+		$AnimatedSprite.animation = "up"
+	elif velocity.y > 0:
+		$AnimatedSprite.animation = "down"
 
 func handle_hit():
 	MOB_HP -= 1
@@ -28,7 +51,7 @@ func handle_hit():
 		queue_free()
 		emit_signal("mob_killed")
 		
-
+		
 func _on_AI_attempt_to_shoot(target):
 	if(attack_cooldown.is_stopped()):
 		var spell_instance = preload("res://spells/Spell.tscn").instance()
@@ -36,3 +59,11 @@ func _on_AI_attempt_to_shoot(target):
 		var spell_dir = (target-spell_origin).normalized()
 		emit_signal("mob_cast_spell", spell_instance, self, spell_origin, spell_dir)
 		attack_cooldown.start()
+
+func _on_AI_chase_target(target_pos):
+	set_mob_direction(target_pos)
+
+func set_mob_direction(target_pos):
+	if(target_pos == Vector2.ZERO):
+		target_pos = self.global_position
+	mob_direction = target_pos - self.global_position
