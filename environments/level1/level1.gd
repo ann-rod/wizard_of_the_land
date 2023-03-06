01544fadfb98d3d31082d01369bea0298f033690
jonnyabdo <102502099+jonnyabdo@users.noreply.github.com>
2023-03-06 13:28:16 -0600

    file reorg and slight changes

---
+++ environments/level1/level1.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+onready var spell_manager = $SpellManager
+onready var player = $Player
+onready var map = $Map
+onready var mob = $Mob
+
+
+func _ready():
+	player.connect("player_cast_spell", spell_manager, "handle_spell_spawned")
+	
+
+func game_over():
+	print("game over!")
+	player.hide() # hide player
+	player.collision_shape.set_deferred("disabled", true) # disable collisions
+	player.disconnect("player_cast_spell", spell_manager, "handle_spell_spawned")
+	
+func _on_Player_player_hp_zero():
+	game_over()
