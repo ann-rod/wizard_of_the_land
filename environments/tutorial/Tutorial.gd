a29e0e029dd3007bd702ad56a472bf7ff2fcb963
hankhayes <105171359+hankhayes@users.noreply.github.com>
2023-03-06 12:26:30 -0600

    added rest of tutorial functionality

--- environments/tutorial/Tutorial.gd
+++ environments/tutorial/Tutorial.gd
@@ -5,18 +5,19 @@ signal enter_area_2
 signal enter_area_3
 
 onready var instruction_label = $TutorialHUD/InstructionsLabel
+onready var tutorial_label = $TutorialHUD/TutorialLabel
+onready var player = $Player
+onready var chest = $Chest
+onready var next_button = $NextButton
 
 var instructions = ['Use WASD to move around', 'Aim with MOUSE and press LEFT CLICK to shoot', 'Avoid enemies to stay alive']
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	chest.hide()
+	next_button.hide()
+	next_button.disabled = true
+	$Chest/Area2D/CollisionShape2D.set_deferred("disabled", true)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -59,3 +60,31 @@ func _on_ThirdArea_area_entered(area):
 
 func _on_ThirdArea_area_exited(area):
 	print('Left 3rd area')
+
+
+func _on_Mob_mob_killed():
+	instruction_label.text = "Collect the chest to complete the level!"
+	chest.show()
+	$SecondArea.monitoring = false
+	$FirstArea.monitoring = false
+	$ThirdArea.monitoring = false
+	$Chest/Area2D/CollisionShape2D.set_deferred("disabled", false)
+	
+
+
+func _on_Chest_chest_opened():
+	print('chest collected')
+	chest.hide()
+	tutorial_label.text = "Congratulations!"
+	instruction_label.text = "You have completed the tutorial!"
+	next_button.show()
+	next_button.disabled = false
+
+
+func _on_NextButton_pressed():
+	pass # Replace with function body.
+
+func is_mouse_over_hud():
+	var next_button_rect = next_button.get_global_rect()
+	var mouse_pos = get_global_mouse_position()
+	return next_button_rect.has_point(mouse_pos)
--- spells/SpellManager.gd
+++ spells/SpellManager.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 func handle_spell_spawned(spell, spellcaster, spell_origin, dir):
+	print('spell!')
 	add_child(spell)
 	spell.global_position = spell_origin
 	spell.init_spell(spellcaster, dir)
