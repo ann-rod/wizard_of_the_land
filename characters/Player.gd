1d093f70fce4845644867ea499fa713aee0b8210
hankhayes <105171359+hankhayes@users.noreply.github.com>
2023-03-24 11:40:06 -0500

    Fixed sticky tree movement

--- characters/Player.gd
+++ characters/Player.gd
@@ -51,7 +51,7 @@ func _process(delta):
 		$AnimatedSprite.stop()
 		
 	#position += velocity * delta
-	move_and_collide(velocity * delta)
+	move_and_slide(velocity)
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 	
@@ -73,7 +73,8 @@ func init_health_bar():
 #	#print("player was hit!")
 #	handle_hit()
 	
-	
+func _physics_process(delta):
+	pass
 	
 
 	
