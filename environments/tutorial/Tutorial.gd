0a01ab205b0e0758a3adf7c83c9e5b8cb7f6b5e7
hankhayes <105171359+hankhayes@users.noreply.github.com>
2023-04-05 17:37:44 -0500

    Updated tutorial HUD

--- environments/tutorial/Tutorial.gd
+++ environments/tutorial/Tutorial.gd
@@ -4,20 +4,12 @@ signal enter_area_1
 signal enter_area_2
 signal enter_area_3
 
-onready var instruction_label = $TutorialHUD/InstructionsLabel
-onready var tutorial_label = $TutorialHUD/TutorialLabel
 onready var player = $Player
-onready var chest = $Chest
-onready var next_button = $NextButton
 
-var instructions = ['Use WASD to move around', 'Aim with MOUSE and press LEFT CLICK to shoot', 'Avoid enemies to stay alive']
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	chest.hide()
-	next_button.hide()
-	next_button.disabled = true
-	$Chest/Area2D/CollisionShape2D.set_deferred("disabled", true)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -27,7 +19,7 @@ func _ready():
 
 func _on_FirstArea_area_entered(area):
 	if area.name == "PlayerArea2D":
-		instruction_label.text = instructions[0]
+		
 		$SecondArea.monitoring = true
 		$FirstArea.monitoring = true
 		print('Entered 1st area!')
@@ -40,7 +32,6 @@ func _on_FirstArea_area_exited(area):
 
 func _on_SecondArea_area_entered(area):
 	if area.name == "PlayerArea2D":
-		instruction_label.text = instructions[1]
 		print('Entered 2nd area')
 		emit_signal('enter_area_2')
 
@@ -51,7 +42,6 @@ func _on_SecondArea_area_exited(area):
 
 func _on_ThirdArea_area_entered(area):
 	if area.name == "PlayerArea2D":
-		instruction_label.text = instructions[2]
 		$SecondArea.monitoring = false
 		$FirstArea.monitoring = false
 		print('Entered 3rd area!')
@@ -63,27 +53,6 @@ func _on_ThirdArea_area_exited(area):
 
 
 func _on_Mob_mob_killed():
-	instruction_label.text = "Collect the chest to complete the level!"
-	chest.show()
 	$SecondArea.monitoring = false
 	$FirstArea.monitoring = false
 	$ThirdArea.monitoring = false
-	$Chest/Area2D/CollisionShape2D.set_deferred("disabled", false)
-	
-
-func _on_Chest_chest_opened():
-	print('chest collected')
-	chest.hide()
-	tutorial_label.text = "Congratulations!"
-	instruction_label.text = "You have completed the tutorial!"
-	next_button.show()
-	next_button.disabled = false
-
-
-func _on_NextButton_pressed():
-	get_tree().change_scene("res://key_scenes/title_screen/TitleScreen.tscn")
-
-func is_mouse_over_hud():
-	var next_button_rect = next_button.get_global_rect()
-	var mouse_pos = get_global_mouse_position()
-	return next_button_rect.has_point(mouse_pos)
---
+++ environments/tutorial/TutorialHUD.gd
@@ -0,0 +1,47 @@
+extends Control
+
+onready var instructions_next = $MessageBackground/InstructionsNext
+onready var instructions_back = $MessageBackground/InstructionsBack
+onready var menu_button = $MenuButton
+onready var page_label = $MessageBackground/PageLabel
+onready var instructions_label = $MessageBackground/InstructionLabel
+
+var instructions = ['Use WASD to move around', 'Aim with MOUSE and press LEFT CLICK to shoot', 'Avoid or shoot enemies to stay alive']
+
+var page_number = 1
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	instructions_back.hide()
+	page_label.text = str(page_number) + " / 3"
+	instructions_label.text = instructions[page_number - 1]
+
+func _on_MenuButton_pressed():
+	get_tree().change_scene("res://key_scenes/title_screen/TitleScreen.tscn")
+
+func _on_InstructionsNext_pressed():
+	if page_number < 4:
+		page_number += 1
+		page_label.text = str(page_number) + " / 3"
+		instructions_label.text = instructions[page_number - 1]
+		check_page_number()
+	
+
+func _on_InstructionsBack_pressed():
+	if page_number > 1:
+		page_number -= 1
+		page_label.text = str(page_number) + " / 3"
+		instructions_label.text = instructions[page_number - 1]
+		check_page_number()
+	
+		
+func check_page_number():
+	if (page_number in [1, 2]):
+		instructions_next.show()
+	else:
+		instructions_next.hide()
+	if (page_number in [2, 3]):
+		instructions_back.show()
+	else:
+		instructions_back.hide()
